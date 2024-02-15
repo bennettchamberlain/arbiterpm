29beb2667f1a7484913c8ac880e36939e84dee8d
bennettchamberlain <chamberlain.bennett@gmail.com>
2024-02-14 21:28:48 -0500

    very good create

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.arbiterpm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:arbiterpm/app/app.dart';
+import 'package:arbiterpm/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
